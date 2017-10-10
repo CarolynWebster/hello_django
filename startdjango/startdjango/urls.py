bf24db4657f1f99588f5fc484dc55e0d17f9335a
vagrant <CarolynWebster@users.noreply.github.com>
2017-10-10 19:17:30 +0000

    Added additional polls app

--- startdjango/startdjango/urls.py
+++ startdjango/startdjango/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls), 
     url(r'^hello/', include('helloworld.urls')),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls), 
 ]
